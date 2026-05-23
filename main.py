a7eaa4d5ba080647fe6a0dc2eb6a99d45edb6ddb
mochazzt <166436942+SoggyBobanana@users.noreply.github.com>
2026-05-23 20:32:16 +0800

    Update main.py, added calculation function for prices and adjust item list features

--- main.py
+++ main.py
@@ -5,24 +5,38 @@ items = []
 print("WELCOME TO EXPENSE TRACKER")
 
 while True:
-    item = input("\nenter item (put 0 to stop)\n")
+    item = input("\nenter item (put x to stop)\n") # enter item name
     
     if item == "":
-        print("empty input not allowed")
+        print("empty input not allowed, please insert your item again")
         continue
     
     if not item.replace(" ", "").isalpha():
-        print("only word allowed")
+        print("only word allowed, please insert your item again")
         continue
     
-    if item == "0":
+    if item == "x" or item == "X":
         break
     
-    items.append(item) # store item in the list
+    try: 
+        price = float(input("Enter price : RM ")) # user input price
+    except ValueError:
+        continue
+    
+    items.append((item, price)) # store item in the list
 
-print("\nitems :")
-for i in items:
-    print("-",i)
+print("\n======= Item List =======")
+
+total = 0 
+
+for item, price in items:
+    print(f" - {item} : RM {price:.2f}")
+    total += price
+    
 print("\nTotal items:", len(items))
+print(f"Total spent : RM {total: .2f}")
+
+
+
 
 
